33a65684176969a3155bf251cc75cc1c20bc6a3a
Lanny MacMillan <lannymacmillan@gmail.com>
2024-02-22 15:54:54 -0500

    basic skills containers setup

--- src/components/skills/Skills.js
+++ src/components/skills/Skills.js
@@ -1,8 +1,16 @@
 import React from "react";
+import { ReactIconColor } from "../assets/icons.js";
 import {
   SkillsContainer,
   Header,
   Skills0,
+  SkillsHeader,
+  ColorAccent,
+  Icon,
+  Title,
+  DescriptionBody,
+  Description,
+  HTMLSnippet,
   Skills1,
   Skills2,
   Skills3,
@@ -14,15 +22,57 @@ const Skills = ({ skills }) => {
       <SkillsContainer ref={skills}>
         <Header>My Skills</Header>
         <Skills0>
-          Software Dev, functional and OOP programming, Javascript, C#,
-          Typescript, Python - Animated screen typing
+          <SkillsHeader>
+            <Icon>
+              <ReactIconColor height={60} width={60} />{" "}
+            </Icon>
+            <Title>
+              <ColorAccent />
+              Software <br />
+              Development{" "}
+            </Title>
+          </SkillsHeader>
+          <DescriptionBody>
+            <HTMLSnippet></HTMLSnippet>
+            <Description>
+              Software Dev, functional and OOP programming, Javascript, C#,
+              Typescript, Python - Animated screen typing
+            </Description>
+          </DescriptionBody>
         </Skills0>
         <Skills1>
-          Frontend Dev- UI - HTML css JS React, Express. Mobile first
-          development mindset - React logo spins
+          <SkillsHeader>
+            <Icon>
+              <ReactIconColor height={60} width={60} />{" "}
+            </Icon>
+            <Title>
+              <ColorAccent background={"purple"} width={"126px"} />
+              Frontend <br />
+              Development{" "}
+            </Title>
+          </SkillsHeader>
+          <DescriptionBody>
+            <HTMLSnippet></HTMLSnippet>
+            <Description>
+              HTML css JS React, Express. Mobile first development mindset
+            </Description>
+          </DescriptionBody>
         </Skills1>
         <Skills2>
-          Mobile Development - React Native, Flutter, Android iOS?
+          <SkillsHeader>
+            <Icon>
+              <ReactIconColor height={60} width={60} />{" "}
+            </Icon>
+            <Title>
+              <ColorAccent background={"blue"} width={"95px"} />
+              Mobile <br />
+              Development{" "}
+            </Title>
+          </SkillsHeader>
+          <DescriptionBody>
+            <HTMLSnippet></HTMLSnippet>
+            <Description>React Native, Flutter, Android iOS</Description>
+          </DescriptionBody>
         </Skills2>
         <Skills3>Game Development - Unity C# developer</Skills3>
       </SkillsContainer>
--- src/components/skills/Skills.styles.js
+++ src/components/skills/Skills.styles.js
@@ -29,25 +29,97 @@ export const Header = styled.div`
   font-optical-sizing: auto;
   font-weight: 800;
   font-style: normal;
-  // background-color: grey;
 `;
 
 export const Skills0 = styled.div`
   grid-area: skills0;
-  background-color: blue;
+  padding: 60px;
+  font-family: "DM Sans", sans-serif;
+  font-optical-sizing: auto;
+  font-weight: 800;
+  font-style: normal;
+  border: 5px solid white;
+  border-right: none;
+`;
+
+export const SkillsHeader = styled.div`
+  display: flex;
+  flex-direction: row;
+  font-size: 28px;
+  font-weight: 900;
+`;
+
+export const Icon = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const ColorAccent = styled.div`
+  position: absolute;
+  margin-bottom: 18px;
+  z-index: -1;
+  width: ${(props) => (props.width ? props.width : "125px")};
+  background: ${(props) => (props.background ? props.background : "red")};
+  height: 10px;
+`;
+
+export const Title = styled.div`
+  display: flex;
+  z-index: 5;
+  width: 100%;
+  align-items: center;
+  padding-left: 10px;
+`;
+
+export const DescriptionBody = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  padding-top: 30px;
+`;
+
+export const Description = styled.div`
+  padding: 10px;
+  // border: 2px solid yellow;
+`;
+
+export const HTMLSnippet = styled.div`
+  padding: 15px;
+  // display: flex;
+  // flex-direction: column;
+  height: 100%;
+  width: 10%;
+  border: 2px solid yellow;
 `;
 
 export const Skills1 = styled.div`
   grid-area: skills1;
-  background-color: green;
+  padding: 60px;
+  font-family: "DM Sans", sans-serif;
+  font-optical-sizing: auto;
+  font-weight: 800;
+  font-style: normal;
+  border: 5px solid white;
+  // outline-style: solid none;
+  // background-color: green;
 `;
 
 export const Skills2 = styled.div`
   grid-area: skills2;
-  background-color: purple;
+  padding: 60px;
+  font-family: "DM Sans", sans-serif;
+  font-optical-sizing: auto;
+  font-weight: 800;
+  font-style: normal;
+  border: 5px solid white;
+  border-left: none;
+  // background-color: purple;
 `;
 
 export const Skills3 = styled.div`
   grid-area: skills3;
+  border: 5px solid white;
+  border-top: none;
   background-color: navy;
 `;
